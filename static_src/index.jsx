e111520fa98f112175f98a75a216c8ada3151ef7
Vladimir Lanfinikov <lanfinikov@gmail.com>
2020-01-27 14:37:01 +0300

    lesson2

--- static_src/index.jsx
+++ static_src/index.jsx
@@ -1,31 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/MessageField';
 
-
-let messages = ['Привет', 'Как дела?'];
-
-const MessageComponent = (props) => <div>{props.text}</div>;
-
-const MessageField = (props) => {
-   return props.messages.map(message => <MessageComponent text={ message } />);
-};
-
-const knopka = <button onClick={f}>Ответить</button>;
-
-function f() {
-   messages.push('Нормально');
-   ReactDOM.render(
-      <MessageField messages={ messages } />,
-      document.getElementById('root'),
-   );
-}
 
 ReactDOM.render(
-   <MessageField messages={ messages } />,
+   <App />,
    document.getElementById('root'),
-);
-
-ReactDOM.render(
-   knopka,
-   document.getElementById('root2'),
 );
